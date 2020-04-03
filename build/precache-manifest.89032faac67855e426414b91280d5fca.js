6772545785d00e5e930dbafa70998ce509f19370
karthik kn <koppakarthik@gmail.com>
2020-04-03 14:03:43 +0530

    changed name

---
+++ build/precache-manifest.89032faac67855e426414b91280d5fca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f9974ae90d7b9908664b24258a20ca",
+    "url": "/react-redux-demo-app/index.html"
+  },
+  {
+    "revision": "ada9dd754f9757e5d478",
+    "url": "/react-redux-demo-app/static/css/main.0e3b3b37.chunk.css"
+  },
+  {
+    "revision": "83788b08290f7d74ab7f",
+    "url": "/react-redux-demo-app/static/js/2.6203575e.chunk.js"
+  },
+  {
+    "revision": "99eecbe0e7fc6292d8355d7acee57e18",
+    "url": "/react-redux-demo-app/static/js/2.6203575e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada9dd754f9757e5d478",
+    "url": "/react-redux-demo-app/static/js/main.42b23d59.chunk.js"
+  },
+  {
+    "revision": "671e5df4823873601f08",
+    "url": "/react-redux-demo-app/static/js/runtime-main.6ae4951b.js"
+  }
+]);
